2b17040d94f8785484a55df37a234f5e92312f5f
Jun-Murakami <contact@bucketrelay.com>
2024-10-06 00:14:14 +0900

    feat: Update routing to use HashRouter instead of BrowserRouter
    The code changes update the routing in the App component to use HashRouter instead of BrowserRouter. This change is made to ensure compatibility with hosting environments that do not support browser history API.

--- src/renderer/src/features/app/App.tsx
+++ src/renderer/src/features/app/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { indexedDb } from '@/indexedDb';
 import { theme, darkTheme } from '@/theme/mui_theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
